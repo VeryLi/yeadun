6c4ff2f42748e19adf72fd89fb23784f703c97db
chen <weili.chen@yeadun.com>
2016-11-29 17:30:40 +0800

    Add

---
+++ src/main/java/com/yeadun/bigdata/platform/client/sss.java
@@ -0,0 +1,7 @@
+package com.yeadun.bigdata.platform.client;
+
+/**
+ * Created by chen on 16-11-29.
+ */
+public class sss {
+}
---
+++ src/test/java/SerdeTest/Test1.java
@@ -0,0 +1,7 @@
+package SerdeTest;
+
+/**
+ * Created by chen on 16-11-29.
+ */
+public class Test1 {
+}
