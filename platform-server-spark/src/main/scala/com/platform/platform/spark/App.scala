7b6f9341319012c7b5caa766e8e8900318ba9c3c
chen <weili.chen@yeadun.com>
2016-12-21 19:13:54 +0800

    YeaDun BigData Frame V1.0-Final

--- platform-server-spark/src/main/scala/com/platform/platform/spark/App.scala
+++ platform-server-spark/src/main/scala/com/platform/platform/spark/App.scala
@@ -1,6 +1,24 @@
 package com.platform.platform.spark
 
+import java.util
+
+import scala.collection.mutable.ArrayBuffer
 
 object App {
-  println( "Hello World!" )
+  def main(args: Array[String]): Unit = {
+    println( "Hello World!" )
+    val a = new util.ArrayList[String]()
+    val ite = Iterator(1,2,3,4)
+    val array = ArrayBuffer[(String, String)]()
+    while(ite.hasNext){
+      val a = ite.next()
+      val key = a + "key"
+      val value = a + "value"
+      array.append((key, value))
+    }
+    val itera = array.iterator
+    println(itera.length)
+
+  }
+
 }
--- platform-server-spark/src/test/scala/com/platform/server/spark/AppTest.scala
+++ platform-server-spark/src/test/scala/com/platform/server/spark/AppTest.scala
@@ -1,11 +1,55 @@
 package com.platform.server.spark
 
+import java.io.File
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.Iterator
+import scala.collection.mutable.ArrayBuffer
+import scala.reflect.io.Directory
+
 class AppTest {
 
 }
 
 object AppTest {
+  val sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkTest")
+  val spark = new SparkContext(sparkConf)
+  val result = new Directory(new File("data/result"))
+  val result2 = new Directory(new File("data/result2"))
 
+  def main(args: Array[String]): Unit = {
+    delResult()
+    val file = spark.textFile("data/testfile")
+    val map1 = file.flatMap(a => a.split(","))
+    val map2 = file.mapPartitions(a => {
+      val array = ArrayBuffer[(String, String)]()
+      var key = ""
+      var value = ""
+      while(a.hasNext){
+        val line = a.next()
+        key = line.split(",")(0)
+        value = line.split(",")(1)
+        array.append((key, value))
+      }
+      array.iterator
+    })
+
+    map1.saveAsTextFile("data/result")
+    map2.saveAsTextFile("data/result2")
+    println("RDD info ==> Partitions : " + map2.getNumPartitions)
+  }
+
+
+  def delResult() = {
+    if ( this.result.exists ) {
+      this.result.deleteRecursively()
+    }
+
+    if ( this.result2.exists ) {
+      this.result2.deleteRecursively()
+    }
+  }
 }
 
 
